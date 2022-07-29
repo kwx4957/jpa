748ecf6e516b24d806272d05cd60b54d2556a0aa
kwx4957 <kwx4957@gmail.com>
2022-07-29 23:36:34 +0900

    init and Jpa study chap.2

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,33 @@
+package hello;
+
+import hello.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("hello");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
